c6765f427f4de4fef576b40294d82f322bf55640
Pedro Martins Dusso <pedro@aegro.com.br>
2025-10-21 11:11:43 -0300

    test: setup Jest testing infrastructure

---
+++ jest.config.js
@@ -0,0 +1,24 @@
+/** @type {import('jest').Config} */
+export default {
+  preset: 'ts-jest/presets/default-esm',
+  testEnvironment: 'node',
+  extensionsToTreatAsEsm: ['.ts'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', {
+      useESM: true,
+      tsconfig: 'tsconfig.test.json'
+    }],
+  },
+  testMatch: ['**/tests/**/*.test.ts'],
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/**/*.d.ts',
+    '!src/**/index.ts',
+  ],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov', 'html'],
+};
---
+++ tests/setup.test.ts
@@ -0,0 +1,5 @@
+describe('Test Infrastructure', () => {
+  test('Jest should be configured correctly', () => {
+    expect(true).toBe(true);
+  });
+});
